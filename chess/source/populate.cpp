e5da4067a54d7f31af14e2c3e600e22f417b6509
Sebastian Carter <Sebastian.Carter@web.de>
2019-10-19 20:05:50 +0200

    Add king moves to find_legal_moves(..)

--- chess/source/populate.cpp
+++ chess/source/populate.cpp
@@ -3,18 +3,61 @@
 namespace Chess {
 namespace detail {
 
+template <Piece>
+std::vector<NodePtr> find_moves(const Node& node, const Coordinate& coordinate);
+
+template <>
+std::vector<NodePtr> find_moves<Piece::k>(const Node& node,
+                                          const Coordinate& location) {
+  /// todo: castling moves are missing
+  constexpr std::array<Coordinate, 8> king_directions{
+      {{1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}, {1, 0}}};
+
+  Chess::State state_before_move{node.state_};
+  state_before_move.turn_ = node.state_.turn_ == GameTree::Player::max
+                                ? GameTree::Player::min
+                                : GameTree::Player::max;
+  state_before_move.plies_++;
+  state_before_move.static_plies_++;
+
+  std::vector<NodePtr> moves{};
+  for (const Coordinate& direction : king_directions) {
+    const Coordinate destination = location + direction;
+
+    /// todo: king is not in check
+    if (IsOnTheBoard(destination) &&
+        IsAPieceOfSide(node.state_.board_.Get(destination),
+                       node.state_.turn_)) {
+      Chess::State state_after_move{state_before_move};
+      state_after_move.board_.Set(location, Piece::e);
+      state_after_move.board_.Set(destination, Piece::k);
+      NodePtr king_move = std::make_unique<Node>(std::move(state_after_move));
+      moves.push_back(std::move(king_move));
+    }
+  }
+
+  return moves;
+}
+
 std::vector<NodePtr> find_legal_moves(const Node& node) {
-  for (const File& file : FILES) {
-    for (const Rank& rank : RANKS) {
-      const Piece piece{node.state_.GetPieceFrom(file, rank)};
+  constexpr std::array<Row, 8> rows{0, 1, 2, 3, 4, 5, 6, 7};
+  constexpr std::array<Column, 8> cols{0, 1, 2, 3, 4, 5, 6, 7};
+
+  std::vector<NodePtr> legal_moves{};
+  for (const Row row : rows) {
+    for (const Column col : cols) {
+      const Piece piece{node.state_.board_.Get({col, row})};
       if (IsAPieceOfSide(piece, node.state_.turn_)) {
-        // find_moves<code>(node, {file, rank});
-        return {};
+        std::vector<NodePtr> piece_moves{
+            find_moves<Piece::k>(node, {col, row})};
+        for (NodePtr& node : piece_moves) {
+          legal_moves.emplace_back(std::move(node));
+        }
       }
     }
   }
 
-  return std::vector<NodePtr>{};
+  return legal_moves;
 }
 
 }  // namespace detail
--- chess/test/populate_unit_tests.cpp
+++ chess/test/populate_unit_tests.cpp
@@ -5,11 +5,31 @@
 namespace Chess {
 namespace {
 
-class FindLegalMovesTest : public detail::EmptyBoardFixture {};
+void PrintNode(const Node& node) {
+  std::cout << "Node:\n";
+  std::cout << node.state_.board_;
 
-TEST_F(FindLegalMovesTest, MustNotThrow) {
-  std::unique_ptr<Node> unit{std::make_unique<Node>(state_)};
-  unit->children_ = detail::find_legal_moves(*unit);
+  if (node.children_.size()) {
+    std::cout << "Children:\n";
+  }
+  for (const std::unique_ptr<Node>& child : node.children_) {
+    std::cout << child->state_.board_;
+  }
+}
+
+class FindLegalMovesTest : public testing::Test {
+ public:
+  void SetUp() override {}
+  void TearDown() override { PrintNode(node_); }
+
+  Node node_{State{}};
+};
+
+TEST_F(FindLegalMovesTest, WithEmptyBoard_ExpectNoMoves) {
+  node_.children_ = detail::find_legal_moves(node_);
+  const std::size_t expected_children{0};
+
+  EXPECT_EQ(expected_children, node_.children_.size());
 }
 
 }  // namespace
