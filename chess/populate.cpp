109e49a2f5433550fd47f938e4966c6749b790f0
Sebastian Carter <Sebastian.Carter@web.de>
2020-01-02 20:16:33 +0100

    Update populate skeleton to object oriented approach

--- chess/populate.cpp
+++ chess/populate.cpp
@@ -1,56 +1,21 @@
 #include "chess/populate.h"
 
+#include <algorithm>
+
 namespace Chess {
-namespace detail {
 
-std::vector<NodePtr> find_legal_moves(const Node& node) {
-  constexpr std::array<Row, 8> rows{0, 1, 2, 3, 4, 5, 6, 7};
-  constexpr std::array<Column, 8> cols{0, 1, 2, 3, 4, 5, 6, 7};
-
+std::vector<NodePtr> collect_moves_from_all_pieces(const Node& node) {
   std::vector<NodePtr> legal_moves{};
-  for (const Row row : rows) {
-    for (const Column col : cols) {
-      const Coordinate location{col, row};
-      const Piece piece{node.state_.board_.Get(location)};
+  for (const SquarePtr& square : node.state_.board_.squares_) {
+    std::vector<NodePtr>
+        piece_moves{};  //= square->FindMoves(coordinate, node);
 
-      std::vector<NodePtr> piece_moves{};
-      switch (piece) {
-        case Piece::e:
-          break;
-        case Piece::K:
-        case Piece::k:
-          piece_moves = find_moves<Piece::k>(node, location);
-          break;
-        case Piece::P:
-        case Piece::p:
-          piece_moves = find_moves<Piece::p>(node, location);
-          break;
-        case Piece::B:
-        case Piece::b:
-          piece_moves = find_moves<Piece::b>(node, location);
-          break;
-        case Piece::N:
-        case Piece::n:
-          piece_moves = find_moves<Piece::n>(node, location);
-          break;
-        case Piece::R:
-        case Piece::r:
-          piece_moves = find_moves<Piece::r>(node, location);
-          break;
-        case Piece::Q:
-        case Piece::q:
-          piece_moves = find_moves<Piece::q>(node, location);
-          break;
-      }
-
-      for (NodePtr& node : piece_moves) {
-        legal_moves.emplace_back(std::move(node));
-      }
+    for (NodePtr& node : piece_moves) {
+      legal_moves.emplace_back(std::move(node));
     }
   }
 
   return legal_moves;
 }
 
-}  // namespace detail
 }  // namespace Chess
--- chess/populate.h
+++ chess/populate.h
@@ -5,133 +5,21 @@
 #include "chess/state.h"
 
 namespace Chess {
-namespace detail {
 
-template <Piece>
-std::vector<NodePtr> find_moves(const Node& node, const Coordinate& coordinate);
+std::vector<NodePtr> collect_moves_from_all_pieces(const Chess::Node&);
 
-template <>
-std::vector<NodePtr> find_moves<Piece::e>(const Node& /*unsused*/,
-                                          const Coordinate& /*unsused*/) {
-  return {};
-}
-
-template <>
-std::vector<NodePtr> find_moves<Piece::k>(const Node& node,
-                                          const Coordinate& location) {
-  const Piece piece{node.state_.board_.Get(location)};
-  if (IsAPieceOfSide(piece, node.state_.turn_)) {
-    /// todo: add castling moves
-    constexpr std::array<Coordinate, 8> king_directions{
-        {{1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}, {1, 0}}};
-
-    Chess::State state_before_move{node.state_};
-    state_before_move.turn_ = node.state_.turn_ == AlphaBeta::Player::max
-                                  ? AlphaBeta::Player::min
-                                  : AlphaBeta::Player::max;
-    state_before_move.plies_++;
-    state_before_move.static_plies_++;
-
-    std::vector<NodePtr> moves{};
-    for (const Coordinate& direction : king_directions) {
-      const Coordinate destination = location + direction;
-
-      /// todo: check that king is not in check
-      if (IsOnTheBoard(destination) &&
-          !IsAPieceOfSide(node.state_.board_.Get(destination),
-                          node.state_.turn_)) {
-        Chess::State state_after_move{state_before_move};
-        state_after_move.board_.Set(destination,
-                                    state_after_move.board_.Get(location));
-        state_after_move.board_.Set(location, Piece::e);
-        moves.emplace_back(std::make_unique<Node>(std::move(state_after_move)));
-      }
-    }
-
-    return moves;
-  } else
-    return {};
-}
-
-template <>
-std::vector<NodePtr> find_moves<Piece::n>(const Node& /*unsused*/,
-                                          const Coordinate& /*unsused*/) {
-  return {};
-}
-
-template <>
-std::vector<NodePtr> find_moves<Piece::b>(const Node& /*unsused*/,
-                                          const Coordinate& /*unsused*/) {
-  return {};
-}
-
-template <>
-std::vector<NodePtr> find_moves<Piece::r>(const Node& /*unsused*/,
-                                          const Coordinate& /*unsused*/) {
-  return {};
-}
-
-template <>
-std::vector<NodePtr> find_moves<Piece::q>(const Node& /*unsused*/,
-                                          const Coordinate& /*unsused*/) {
-  return {};
-}
-
-template <>
-std::vector<NodePtr> find_moves<Piece::K>(const Node& node,
-                                          const Coordinate& location) {
-  return find_moves<Piece::k>(node, location);
-}
-
-template <>
-std::vector<NodePtr> find_moves<Piece::N>(const Node& node,
-                                          const Coordinate& location) {
-  return find_moves<Piece::n>(node, location);
-}
-
-template <>
-std::vector<NodePtr> find_moves<Piece::B>(const Node& node,
-                                          const Coordinate& location) {
-  return find_moves<Piece::b>(node, location);
-}
-
-template <>
-std::vector<NodePtr> find_moves<Piece::R>(const Node& node,
-                                          const Coordinate& location) {
-  return find_moves<Piece::r>(node, location);
-}
-
-template <>
-std::vector<NodePtr> find_moves<Piece::Q>(const Node& node,
-                                          const Coordinate& location) {
-  return find_moves<Piece::q>(node, location);
-}
-
-template <>
-std::vector<NodePtr> find_moves<Piece::p>(const Node& /*unsused*/,
-                                          const Coordinate& /*unsused*/) {
-  return {};
-}
-
-template <>
-std::vector<NodePtr> find_moves<Piece::P>(const Node& /*unsused*/,
-                                          const Coordinate& /*unsused*/) {
-  return {};
-}
-
-std::vector<NodePtr> find_legal_moves(const Node& node);
-
-}  // namespace detail
 }  // namespace Chess
 
 namespace AlphaBeta {
 
 template <>
-void populate(Node<Chess::State>& node, const int depth) {
-  if (!depth) {
-    node.children_ = Chess::detail::find_legal_moves(node);
-  } else {
-    for (std::unique_ptr<Node<Chess::State>>& child : node.children_) {
+void populate(Chess::Node& node, const int depth) {
+  if (depth) {
+    if (!node.children_.size()) {
+      node.children_ = Chess::collect_moves_from_all_pieces(node);
+    }
+
+    for (Chess::NodePtr& child : node.children_) {
       populate(*child, depth - 1);
     }
   }
