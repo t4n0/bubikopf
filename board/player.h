84e38a61745fd7595e39273dee8d28bcb611013c
Sebastian Carter <Sebastian.Carter@web.de>
2020-03-14 17:38:59 +0100

    Implement Player negation operator

--- board/player.h
+++ board/player.h
@@ -2,10 +2,16 @@
 #define BOARD_PLAYER_H
 
 namespace Chess {
+
 enum class Player {
   min,  // flag for minimizing player (e.g turn, victory, etc.)
   max,  // flag for maximizing player (e.g turn, victory, etc.)
 };
+
+inline Player operator!(const Player& player) {
+  return player == Player::max ? Player::min : Player::max;
 }
+
+}  // namespace Chess
 
 #endif
---
+++ board/test/player_unit_tests.cpp
@@ -0,0 +1,23 @@
+#include "board/player.h"
+
+#include <gtest/gtest.h>
+
+namespace Chess {
+namespace {
+
+TEST(PlayerTest, NegationOperator) {
+  // Setup
+  const Player maximizing_player = Player::max;
+  const Player minimizing_player = Player::min;
+
+  // Call
+  const Player negation_of_maximizing_player = !maximizing_player;
+  const Player negation_of_minimizing_player = !minimizing_player;
+
+  // Expect
+  EXPECT_EQ(negation_of_maximizing_player, Player::min);
+  EXPECT_EQ(negation_of_minimizing_player, Player::max);
+}
+
+}  // namespace
+}  // namespace Chess
