a0c5aea639f9b8db41698b49a4fb3dd74a095b46
Sebastian Carter <Sebastian.Carter@web.de>
2020-03-21 14:35:15 +0100

    Implement puzzle test "find mate in one"

--- search/test/puzzle_tests.cpp
+++ search/test/puzzle_tests.cpp
@@ -1,5 +1,6 @@
 #include "board/utilities.h"
 #include "search/evaluate.h"
+#include "search/minimax.h"
 #include "search/populate.h"
 
 #include <gtest/gtest.h>
@@ -7,16 +8,32 @@
 namespace Chess {
 namespace {
 
-TEST(Evaluate_Fixture, GivenScholarsMate_ExpectCorrectDefence) {
-  Node node{SetUpBoard()};
-  node.state_.board_.SwapSquares(1, 18);
-  node.state_.board_.SwapSquares(0, 1);
-  node.state_.board_.SwapSquares(34, 61);
-  node.state_.board_.SwapSquares(36, 52);
-  node.state_.board_.SwapSquares(45, 59);
-  node.state_.turn_ = Player::min;
+struct Puzzle_Fixture : public ::testing::Test {
+  Node root_node_{SetUpEmptyBoard()};
+  Board& root_board_{root_node_.state_.board_};
+  const SquareBehaviourPool& pool_{root_node_.state_.pool_};
+};
 
-  populate(node, 5);
+TEST_F(Puzzle_Fixture, GivenMateInOne_ExpectWin) {
+  // Setup
+  root_board_.Set(ToIdx('a', 8), pool_.GetKing(Player::min));
+  root_board_.Set(ToIdx('h', 1), pool_.GetKing(Player::max));
+  root_board_.Set(ToIdx('b', 1), pool_.GetRook(Player::max));
+  root_board_.Set(ToIdx('c', 2), pool_.GetQueen(Player::max));
+  std::cout << root_node_.state_ << '\n';
+
+  const int DEPTH = 3;
+  populate(root_node_, DEPTH);
+  std::cout << "Tree depth " << DEPTH << " generated " << CountNodes(root_node_)
+            << " nodes\n";
+
+  // Call
+  const Evaluation evaluation = minimax(root_node_, DEPTH, MIN_EVAL, MAX_EVAL);
+  std::cout << "Final evaluation " << evaluation << '\n';
+
+  // Expect
+  const Evaluation minimum_expected_evaluation{99.0F};
+  EXPECT_GT(evaluation, minimum_expected_evaluation);
 }
 
 }  // namespace
