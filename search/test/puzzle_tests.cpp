80f278c6d23824c981c9d608f591388672df77e3
Sebastian Carter <Sebastian.Carter@web.de>
2020-03-21 14:41:15 +0100

    Implement puzzle test "find mate in one" for black

--- search/test/puzzle_tests.cpp
+++ search/test/puzzle_tests.cpp
@@ -14,12 +14,13 @@ struct Puzzle_Fixture : public ::testing::Test {
   const SquareBehaviourPool& pool_{root_node_.state_.pool_};
 };
 
-TEST_F(Puzzle_Fixture, GivenMateInOne_ExpectWin) {
+TEST_F(Puzzle_Fixture, GivenMateInOneForWhite_ExpectWhiteWin) {
   // Setup
   root_board_.Set(ToIdx('a', 8), pool_.GetKing(Player::min));
   root_board_.Set(ToIdx('h', 1), pool_.GetKing(Player::max));
   root_board_.Set(ToIdx('b', 1), pool_.GetRook(Player::max));
   root_board_.Set(ToIdx('c', 2), pool_.GetQueen(Player::max));
+  root_node_.state_.turn_ = Player::max;
   std::cout << root_node_.state_ << '\n';
 
   const int DEPTH = 3;
@@ -34,6 +35,29 @@ TEST_F(Puzzle_Fixture, GivenMateInOne_ExpectWin) {
   // Expect
   const Evaluation minimum_expected_evaluation{99.0F};
   EXPECT_GT(evaluation, minimum_expected_evaluation);
+}
+
+TEST_F(Puzzle_Fixture, GivenMateInOneForBlack_ExpectBlackWin) {
+  // Setup
+  root_board_.Set(ToIdx('a', 8), pool_.GetKing(Player::max));
+  root_board_.Set(ToIdx('h', 1), pool_.GetKing(Player::min));
+  root_board_.Set(ToIdx('b', 1), pool_.GetRook(Player::min));
+  root_board_.Set(ToIdx('c', 2), pool_.GetQueen(Player::min));
+  root_node_.state_.turn_ = Player::min;
+  std::cout << root_node_.state_ << '\n';
+
+  const int DEPTH = 3;
+  populate(root_node_, DEPTH);
+  std::cout << "Tree depth " << DEPTH << " generated " << CountNodes(root_node_)
+            << " nodes\n";
+
+  // Call
+  const Evaluation evaluation = minimax(root_node_, DEPTH, MIN_EVAL, MAX_EVAL);
+  std::cout << "Final evaluation " << evaluation << '\n';
+
+  // Expect
+  const Evaluation minimum_expected_evaluation{-99.0F};
+  EXPECT_LT(evaluation, minimum_expected_evaluation);
 }
 
 }  // namespace
