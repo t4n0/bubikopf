e01e8f0ba8124f853c92f0f3590124683ccf7cbd
Sebastian Carter <Sebastian.Carter@web.de>
2020-03-28 23:01:10 +0100

    Refactor lichess_connector to single threaded process

--- play/lichess_connector.py
+++ play/lichess_connector.py
@@ -1,29 +1,50 @@
-import threading
 from berserk.session import TokenSession
 from berserk.clients import Bots
 from pathlib import Path
-GAME_RUNNING = False
+import bubikopf
 
 
-class Game(threading.Thread):
-
-    def __init__(self, client, game_id, **kwargs):
-        super().__init__(**kwargs)
-        self.stream = client.bots.stream_game_state(game_id)
-
-    def run(self):
-        for event in self.stream:
-            if event['type'] == 'gameState':
-                pass
-
-if __name__ == "__main__":
+def ConnectToLichess():
     with open(str(Path.home()) + '/.lichess_token') as f:
         token = f.read().rstrip('\n')
-    bot = Bots(TokenSession(token))
+    return Bots(TokenSession(token))
 
-    for event in bot.stream_incoming_events():
+
+def SendMove(lc_api, game_id, move):
+    NULL_MOVE = '0000'
+    if move != NULL_MOVE:
+        lc_api.make_move(game_id, move)
+
+
+def IsBubikopfWhite(white_player):
+    BUBIKOPF_LICHESS_NAME = 'bubik0pf'
+    return white_player == BUBIKOPF_LICHESS_NAME
+
+
+if __name__ == "__main__":
+    lc_api = ConnectToLichess()
+    bk_api = bubikopf.LichessInteractor()
+    print('Waiting for challenges')
+
+    for event in lc_api.stream_incoming_events():
+
         if event['type'] == 'challenge':
-            if not GAME_RUNNING:
-                bot.accept_challenge(event['challenge']['id'])
-            else:
-                bot.decline_challenge(event['challenge']['id'])
+            print("Accepted new challenge against " +
+                  event['challenge']['challenger']['name'])
+            lc_api.accept_challenge(event['challenge']['id'])
+
+        elif event['type'] == 'gameStart':
+            game_id = event['game']['id']
+            print('Starting game ' + game_id)
+
+            for event in lc_api.stream_game_state(event['game']['id']):
+
+                if event['type'] == 'gameFull':
+                    play_as_white = IsBubikopfWhite(event['white']['name'])
+                    move = bk_api.StartGame(play_as_white)
+                elif event['type'] == 'gameState':
+                    move = bk_api.RespondTo(event['moves'])
+                SendMove(lc_api, game_id, move)
+
+            print('Game ' + game_id + ' is over')
+            print('Waiting for challenges')
