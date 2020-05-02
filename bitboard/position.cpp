1ef5e34d11825048fa8a61c47218a2cb214d66ef
Sebastian Carter <Sebastian.Carter@web.de>
2020-05-02 12:57:18 +0200

    Add skeletons for MakeMove and UnmakeMove

--- bitboard/position.cpp
+++ bitboard/position.cpp
@@ -6,4 +6,12 @@ bool PositionWithBitboards::WhiteToMove() {
   return boards[BOARD_IDX_EXTRAS] & BOARD_MASK_WHITE_TURN;
 }
 
+void PositionWithBitboards::MakeMove(move_t /*unused*/) {
+  // TODO: implement
+}
+
+void PositionWithBitboards::UnmakeMove(move_t /*unused*/) {
+  // TODO: implement
+}
+
 }  // namespace Chess
