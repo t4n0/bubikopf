6d20ad8948afc200b2c1e29b31bfdfe5d4e23cc8
Sebastian Carter <Sebastian.Carter@web.de>
2020-05-03 13:04:54 +0200

    Define global square constants

---
+++ bitboard/squares.h
@@ -0,0 +1,82 @@
+#ifndef BITBOARD_SQUARES_H
+#define BITBOARD_SQUARES_H
+
+#include "bitboard/basic_type_declarations.h"
+
+namespace Chess {
+
+constexpr bitboard_t H1 = 1ULL << 0;
+constexpr bitboard_t G1 = 1ULL << 1;
+constexpr bitboard_t F1 = 1ULL << 2;
+constexpr bitboard_t E1 = 1ULL << 3;
+constexpr bitboard_t D1 = 1ULL << 4;
+constexpr bitboard_t C1 = 1ULL << 5;
+constexpr bitboard_t B1 = 1ULL << 6;
+constexpr bitboard_t A1 = 1ULL << 7;
+
+constexpr bitboard_t H2 = 1ULL << 8;
+constexpr bitboard_t G2 = 1ULL << 9;
+constexpr bitboard_t F2 = 1ULL << 10;
+constexpr bitboard_t E2 = 1ULL << 11;
+constexpr bitboard_t D2 = 1ULL << 12;
+constexpr bitboard_t C2 = 1ULL << 13;
+constexpr bitboard_t B2 = 1ULL << 14;
+constexpr bitboard_t A2 = 1ULL << 15;
+
+constexpr bitboard_t H3 = 1ULL << 16;
+constexpr bitboard_t G3 = 1ULL << 17;
+constexpr bitboard_t F3 = 1ULL << 18;
+constexpr bitboard_t E3 = 1ULL << 19;
+constexpr bitboard_t D3 = 1ULL << 20;
+constexpr bitboard_t C3 = 1ULL << 21;
+constexpr bitboard_t B3 = 1ULL << 22;
+constexpr bitboard_t A3 = 1ULL << 23;
+
+constexpr bitboard_t H4 = 1ULL << 24;
+constexpr bitboard_t G4 = 1ULL << 25;
+constexpr bitboard_t F4 = 1ULL << 26;
+constexpr bitboard_t E4 = 1ULL << 27;
+constexpr bitboard_t D4 = 1ULL << 28;
+constexpr bitboard_t C4 = 1ULL << 29;
+constexpr bitboard_t B4 = 1ULL << 30;
+constexpr bitboard_t A4 = 1ULL << 31;
+
+constexpr bitboard_t H5 = 1ULL << 32;
+constexpr bitboard_t G5 = 1ULL << 33;
+constexpr bitboard_t F5 = 1ULL << 34;
+constexpr bitboard_t E5 = 1ULL << 35;
+constexpr bitboard_t D5 = 1ULL << 36;
+constexpr bitboard_t C5 = 1ULL << 37;
+constexpr bitboard_t B5 = 1ULL << 38;
+constexpr bitboard_t A5 = 1ULL << 39;
+
+constexpr bitboard_t H6 = 1ULL << 40;
+constexpr bitboard_t G6 = 1ULL << 41;
+constexpr bitboard_t F6 = 1ULL << 42;
+constexpr bitboard_t E6 = 1ULL << 43;
+constexpr bitboard_t D6 = 1ULL << 44;
+constexpr bitboard_t C6 = 1ULL << 45;
+constexpr bitboard_t B6 = 1ULL << 46;
+constexpr bitboard_t A6 = 1ULL << 47;
+
+constexpr bitboard_t H7 = 1ULL << 48;
+constexpr bitboard_t G7 = 1ULL << 49;
+constexpr bitboard_t F7 = 1ULL << 50;
+constexpr bitboard_t E7 = 1ULL << 51;
+constexpr bitboard_t D7 = 1ULL << 52;
+constexpr bitboard_t C7 = 1ULL << 53;
+constexpr bitboard_t B7 = 1ULL << 54;
+constexpr bitboard_t A7 = 1ULL << 55;
+
+constexpr bitboard_t H8 = 1ULL << 56;
+constexpr bitboard_t G8 = 1ULL << 57;
+constexpr bitboard_t F8 = 1ULL << 58;
+constexpr bitboard_t E8 = 1ULL << 59;
+constexpr bitboard_t D8 = 1ULL << 60;
+constexpr bitboard_t C8 = 1ULL << 61;
+constexpr bitboard_t B8 = 1ULL << 62;
+constexpr bitboard_t A8 = 1ULL << 63;
+
+}  // namespace Chess
+
+#endif
---
+++ bitboard/test/squares_unit_tests.cpp
@@ -0,0 +1,27 @@
+#include "bitboard/squares.h"
+
+#include <gtest/gtest.h>
+
+#include <array>
+#include <limits>
+
+namespace Chess {
+namespace {
+
+const std::array<bitboard_t, 64> ALL_SQUARES{
+    H1, G1, F1, E1, D1, C1, B1, A1, H2, G2, F2, E2, D2, C2, B2, A2,
+    H3, G3, F3, E3, D3, C3, B3, A3, H4, G4, F4, E4, D4, C4, B4, A4,
+    H5, G5, F5, E5, D5, C5, B5, A5, H6, G6, F6, E6, D6, C6, B6, A6,
+    H7, G7, F7, E7, D7, C7, B7, A7, H8, G8, F8, E8, D8, C8, B8, A8,
+};
+
+TEST(SquaresTest, GivenXorOverAllSquaers_ExpectUnderlyingTypeIsMax) {
+  bitboard_t xor_over_all_squares = 0;
+  for (const bitboard_t square : ALL_SQUARES) {
+    xor_over_all_squares ^= square;
+  }
+  EXPECT_EQ(xor_over_all_squares, std::numeric_limits<bitboard_t>::max());
+}
+
+}  // namespace
+}  // namespace Chess
