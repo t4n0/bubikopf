beb885de23da5b1d3fcdd3c1c9ee6fa9784ac7d1
Sebastian Carter <Sebastian.Carter@web.de>
2020-05-04 22:49:58 +0200

    Add trailing zero count target function

--- hardware/trailing_zeros_count.h
+++ hardware/trailing_zeros_count.h
@@ -1,14 +1,14 @@
 #ifndef HARDWARE_POPULATION_COUNT_H
 #define HARDWARE_POPULATION_COUNT_H
 
-#include <nmmintrin.h>
+#include <immintrin.h>
 
 #include <cstdint>
 
 namespace Chess {
 
-inline long long popcnt(const std::uint64_t board) {
-  return _mm_popcnt_u64(board);
+inline unsigned long long tzcnt(const std::uint64_t board) {
+  return _tzcnt_u64(board);
 }
 
 }  // namespace Chess
