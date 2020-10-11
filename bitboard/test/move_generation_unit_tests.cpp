9f74419a34938b983b143527f11b711fc951cabe
Sebastian Carter <Sebastian.Carter@web.de>
2020-10-11 11:41:58 +0200

    Generalize naming of test fixture

--- bitboard/test/move_generation_unit_tests.cpp
+++ bitboard/test/move_generation_unit_tests.cpp
@@ -10,7 +10,7 @@ namespace Chess
 namespace
 {
 
-struct PawnMoveGenerationTestVector
+struct MoveGenerationTestParameter
 {
     Bitboard extras;
     std::vector<std::pair<std::size_t, Bitboard>> white_pieces;
@@ -18,7 +18,7 @@ struct PawnMoveGenerationTestVector
     std::vector<Bitmove> expected_moves;
 };
 
-class PawnMoveGenerationFixture : public ::testing::TestWithParam<PawnMoveGenerationTestVector>
+class MoveGenerationTestFixture : public ::testing::TestWithParam<MoveGenerationTestParameter>
 {
   public:
     void SetUp() override { position_[BOARD_IDX_EXTRAS] = BOARD_MASK_CASTLING; }
@@ -28,7 +28,7 @@ class PawnMoveGenerationFixture : public ::testing::TestWithParam<PawnMoveGenera
     MoveList move_list_{};
 };
 
-TEST_P(PawnMoveGenerationFixture, GivenAtomicPosition_ExpectAllLegalPawnMoves)
+TEST_P(MoveGenerationTestFixture, GivenAtomicPosition_ExpectAllPseudoLegalMoves)
 {
     position_[BOARD_IDX_EXTRAS] |= GetParam().extras;
 
@@ -55,36 +55,36 @@ TEST_P(PawnMoveGenerationFixture, GivenAtomicPosition_ExpectAllLegalPawnMoves)
 }
 
 // white pawn moves
-const PawnMoveGenerationTestVector kWhitePawnSinglePush{
+const MoveGenerationTestParameter kWhitePawnSinglePush{
     BOARD_MASK_WHITE_TURN,
     {{PAWN, E5}},
     {},
     {ComposeMove(tzcnt(E5), tzcnt(E6), PAWN, NO_CAPTURE, NO_PROMOTION, MOVE_VALUE_TYPE_PAWN_PUSH)}};
-const PawnMoveGenerationTestVector kWhitePawnBlockedMidfield{BOARD_MASK_WHITE_TURN, {{PAWN, D5}}, {{PAWN, D6}}, {}};
-const PawnMoveGenerationTestVector kWhitePawnOnStartRow{
+const MoveGenerationTestParameter kWhitePawnBlockedMidfield{BOARD_MASK_WHITE_TURN, {{PAWN, D5}}, {{PAWN, D6}}, {}};
+const MoveGenerationTestParameter kWhitePawnOnStartRow{
     BOARD_MASK_WHITE_TURN,
     {{PAWN, A2}},
     {},
     {ComposeMove(tzcnt(A2), tzcnt(A3), PAWN, NO_CAPTURE, NO_PROMOTION, MOVE_VALUE_TYPE_PAWN_PUSH),
      ComposeMove(tzcnt(A2), tzcnt(A4), PAWN, NO_CAPTURE, NO_PROMOTION, MOVE_VALUE_TYPE_PAWN_DOUBLE_PUSH)}};
-const PawnMoveGenerationTestVector kWhitePawnBlockedOnStartRow{BOARD_MASK_WHITE_TURN, {{PAWN, C2}}, {{PAWN, C3}}, {}};
-const PawnMoveGenerationTestVector kWhitePawnCaptures{
+const MoveGenerationTestParameter kWhitePawnBlockedOnStartRow{BOARD_MASK_WHITE_TURN, {{PAWN, C2}}, {{PAWN, C3}}, {}};
+const MoveGenerationTestParameter kWhitePawnCaptures{
     BOARD_MASK_WHITE_TURN,
     {{PAWN, C5}},
     {{PAWN, C6}, {KNIGHT, B6}, {ROOK, D6}},
     {ComposeMove(tzcnt(C5), tzcnt(B6), PAWN, KNIGHT, NO_PROMOTION, MOVE_VALUE_TYPE_CAPTURE),
      ComposeMove(tzcnt(C5), tzcnt(D6), PAWN, ROOK, NO_PROMOTION, MOVE_VALUE_TYPE_CAPTURE)}};
-const PawnMoveGenerationTestVector kWhitePawnEnPassentOptionRight{
+const MoveGenerationTestParameter kWhitePawnEnPassentOptionRight{
     BOARD_MASK_WHITE_TURN | (static_cast<Bitboard>(tzcnt(F6)) << BOARD_SHIFT_EN_PASSENT),
     {{PAWN, E5}},
     {{PAWN, E6}, {PAWN, F5}},
     {ComposeMove(tzcnt(E5), tzcnt(F6), PAWN, PAWN, NO_PROMOTION, MOVE_VALUE_TYPE_EN_PASSENT_CAPTURE)}};
-const PawnMoveGenerationTestVector kWhitePawnEnPassentOptionLeft{
+const MoveGenerationTestParameter kWhitePawnEnPassentOptionLeft{
     BOARD_MASK_WHITE_TURN | (static_cast<Bitboard>(tzcnt(D6)) << BOARD_SHIFT_EN_PASSENT),
     {{PAWN, E5}},
     {{PAWN, E6}, {PAWN, D5}},
     {ComposeMove(tzcnt(E5), tzcnt(D6), PAWN, PAWN, NO_PROMOTION, MOVE_VALUE_TYPE_EN_PASSENT_CAPTURE)}};
-const PawnMoveGenerationTestVector kWhitePawnPushToPromotion{
+const MoveGenerationTestParameter kWhitePawnPushToPromotion{
     BOARD_MASK_WHITE_TURN,
     {{PAWN, E7}},
     {},
@@ -92,7 +92,7 @@ const PawnMoveGenerationTestVector kWhitePawnPushToPromotion{
      ComposeMove(tzcnt(E7), tzcnt(E8), PAWN, NO_CAPTURE, ROOK, MOVE_VALUE_TYPE_PROMOTION),
      ComposeMove(tzcnt(E7), tzcnt(E8), PAWN, NO_CAPTURE, KNIGHT, MOVE_VALUE_TYPE_PROMOTION),
      ComposeMove(tzcnt(E7), tzcnt(E8), PAWN, NO_CAPTURE, BISHOP, MOVE_VALUE_TYPE_PROMOTION)}};
-const PawnMoveGenerationTestVector kWhitePawnPushToPromotionWithCapture{
+const MoveGenerationTestParameter kWhitePawnPushToPromotionWithCapture{
     BOARD_MASK_WHITE_TURN,
     {{PAWN, D7}},
     {{KING, D8}, {BISHOP, C8}, {KNIGHT, E8}},
@@ -106,36 +106,36 @@ const PawnMoveGenerationTestVector kWhitePawnPushToPromotionWithCapture{
      ComposeMove(tzcnt(D7), tzcnt(E8), PAWN, KNIGHT, BISHOP, MOVE_VALUE_TYPE_PROMOTION)}};
 
 // black pawn moves
-const PawnMoveGenerationTestVector kBlackPawnSinglePush{
+const MoveGenerationTestParameter kBlackPawnSinglePush{
     {},
     {},
     {{PAWN, E5}},
     {ComposeMove(tzcnt(E5), tzcnt(E4), PAWN, NO_CAPTURE, NO_PROMOTION, MOVE_VALUE_TYPE_PAWN_PUSH)}};
-const PawnMoveGenerationTestVector kBlackPawnBlockedMidfield{{}, {{PAWN, D5}}, {{PAWN, D6}}, {}};
-const PawnMoveGenerationTestVector kBlackPawnOnStartRow{
+const MoveGenerationTestParameter kBlackPawnBlockedMidfield{{}, {{PAWN, D5}}, {{PAWN, D6}}, {}};
+const MoveGenerationTestParameter kBlackPawnOnStartRow{
     {},
     {},
     {{PAWN, G7}},
     {ComposeMove(tzcnt(G7), tzcnt(G6), PAWN, NO_CAPTURE, NO_PROMOTION, MOVE_VALUE_TYPE_PAWN_PUSH),
      ComposeMove(tzcnt(G7), tzcnt(G5), PAWN, NO_CAPTURE, NO_PROMOTION, MOVE_VALUE_TYPE_PAWN_DOUBLE_PUSH)}};
-const PawnMoveGenerationTestVector kBlackPawnBlockedOnStartRow{{}, {{PAWN, H6}}, {{PAWN, H7}}, {}};
-const PawnMoveGenerationTestVector kBlackPawnCaptures{
+const MoveGenerationTestParameter kBlackPawnBlockedOnStartRow{{}, {{PAWN, H6}}, {{PAWN, H7}}, {}};
+const MoveGenerationTestParameter kBlackPawnCaptures{
     {},
     {{PAWN, C4}, {KNIGHT, B4}, {ROOK, D4}},
     {{PAWN, C5}},
     {ComposeMove(tzcnt(C5), tzcnt(B4), PAWN, KNIGHT, NO_PROMOTION, MOVE_VALUE_TYPE_CAPTURE),
      ComposeMove(tzcnt(C5), tzcnt(D4), PAWN, ROOK, NO_PROMOTION, MOVE_VALUE_TYPE_CAPTURE)}};
-const PawnMoveGenerationTestVector kBlackPawnEnPassentOptionRight{
+const MoveGenerationTestParameter kBlackPawnEnPassentOptionRight{
     (static_cast<Bitboard>(tzcnt(E3)) << BOARD_SHIFT_EN_PASSENT),
     {{PAWN, E4}, {PAWN, F3}},
     {{PAWN, F4}},
     {ComposeMove(tzcnt(F4), tzcnt(E3), PAWN, PAWN, NO_PROMOTION, MOVE_VALUE_TYPE_EN_PASSENT_CAPTURE)}};
-const PawnMoveGenerationTestVector kBlackPawnEnPassentOptionLeft{
+const MoveGenerationTestParameter kBlackPawnEnPassentOptionLeft{
     (static_cast<Bitboard>(tzcnt(G3)) << BOARD_SHIFT_EN_PASSENT),
     {{PAWN, G4}, {PAWN, F3}},
     {{PAWN, F4}},
     {ComposeMove(tzcnt(F4), tzcnt(G3), PAWN, PAWN, NO_PROMOTION, MOVE_VALUE_TYPE_EN_PASSENT_CAPTURE)}};
-const PawnMoveGenerationTestVector kBlackPawnPushToPromotion{
+const MoveGenerationTestParameter kBlackPawnPushToPromotion{
     {},
     {},
     {{PAWN, A2}},
@@ -143,7 +143,7 @@ const PawnMoveGenerationTestVector kBlackPawnPushToPromotion{
      ComposeMove(tzcnt(A2), tzcnt(A1), PAWN, NO_CAPTURE, ROOK, MOVE_VALUE_TYPE_PROMOTION),
      ComposeMove(tzcnt(A2), tzcnt(A1), PAWN, NO_CAPTURE, KNIGHT, MOVE_VALUE_TYPE_PROMOTION),
      ComposeMove(tzcnt(A2), tzcnt(A1), PAWN, NO_CAPTURE, BISHOP, MOVE_VALUE_TYPE_PROMOTION)}};
-const PawnMoveGenerationTestVector kBlackPawnPushToPromotionWithCapture{
+const MoveGenerationTestParameter kBlackPawnPushToPromotionWithCapture{
     {},
     {{KING, D1}, {BISHOP, C1}, {KNIGHT, E1}},
     {{PAWN, D2}},
@@ -156,29 +156,26 @@ const PawnMoveGenerationTestVector kBlackPawnPushToPromotionWithCapture{
      ComposeMove(tzcnt(D2), tzcnt(E1), PAWN, KNIGHT, KNIGHT, MOVE_VALUE_TYPE_PROMOTION),
      ComposeMove(tzcnt(D2), tzcnt(E1), PAWN, KNIGHT, BISHOP, MOVE_VALUE_TYPE_PROMOTION)}};
 
-const std::vector<PawnMoveGenerationTestVector> kAtomicPositionsIncludingAllPawnMoves{
-    kWhitePawnSinglePush,
-    kWhitePawnBlockedMidfield,
-    kWhitePawnOnStartRow,
-    kWhitePawnBlockedOnStartRow,
-    kWhitePawnCaptures,
-    kWhitePawnEnPassentOptionRight,
-    kWhitePawnEnPassentOptionLeft,
-    kWhitePawnPushToPromotion,
-    kWhitePawnPushToPromotionWithCapture,
-    kBlackPawnSinglePush,
-    kBlackPawnBlockedMidfield,
-    kBlackPawnOnStartRow,
-    kBlackPawnBlockedOnStartRow,
-    kBlackPawnCaptures,
-    kBlackPawnEnPassentOptionRight,
-    kBlackPawnEnPassentOptionLeft,
-    kBlackPawnPushToPromotion,
-    kBlackPawnPushToPromotionWithCapture};
-
-INSTANTIATE_TEST_SUITE_P(AtomicPositionsIncludingAllPawnMoves,
-                         PawnMoveGenerationFixture,
-                         ::testing::ValuesIn(kAtomicPositionsIncludingAllPawnMoves));
+INSTANTIATE_TEST_SUITE_P(AllAtomicPawnPositions,
+                         MoveGenerationTestFixture,
+                         ::testing::ValuesIn({kWhitePawnSinglePush,
+                                              kWhitePawnBlockedMidfield,
+                                              kWhitePawnOnStartRow,
+                                              kWhitePawnBlockedOnStartRow,
+                                              kWhitePawnCaptures,
+                                              kWhitePawnEnPassentOptionRight,
+                                              kWhitePawnEnPassentOptionLeft,
+                                              kWhitePawnPushToPromotion,
+                                              kWhitePawnPushToPromotionWithCapture,
+                                              kBlackPawnSinglePush,
+                                              kBlackPawnBlockedMidfield,
+                                              kBlackPawnOnStartRow,
+                                              kBlackPawnBlockedOnStartRow,
+                                              kBlackPawnCaptures,
+                                              kBlackPawnEnPassentOptionRight,
+                                              kBlackPawnEnPassentOptionLeft,
+                                              kBlackPawnPushToPromotion,
+                                              kBlackPawnPushToPromotionWithCapture}));
 
 }  // namespace
 }  // namespace Chess
