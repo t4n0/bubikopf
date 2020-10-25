81948b2950f427085fb1e00c596003e245d2e5de
Sebastian Carter <Sebastian.Carter@web.de>
2020-10-25 12:53:11 +0100

    Introduce constants for ranks and files

--- bitboard/squares.h
+++ bitboard/squares.h
@@ -83,9 +83,27 @@ constexpr Bitboard C8 = Bitboard{1} << 61;
 constexpr Bitboard B8 = Bitboard{1} << 62;
 constexpr Bitboard A8 = Bitboard{1} << 63;
 
-constexpr Bitboard START_RANK_WHITE = A2 | B2 | C2 | D2 | E2 | F2 | G2 | H2;
-constexpr Bitboard START_RANK_BLACK = A7 | B7 | C7 | D7 | E7 | F7 | G7 | H7;
-constexpr Bitboard PROMOTION_RANKS = A8 | B8 | C8 | D8 | E8 | F8 | G8 | H8 | A1 | B1 | C1 | D1 | E1 | F1 | G1 | H1;
+constexpr Bitboard RANK_1 = A1 | B1 | C1 | D1 | E1 | F1 | G1 | H1;
+constexpr Bitboard RANK_2 = A2 | B2 | C2 | D2 | E2 | F2 | G2 | H2;
+constexpr Bitboard RANK_3 = A3 | B3 | C3 | D3 | E3 | F3 | G3 | H3;
+constexpr Bitboard RANK_4 = A4 | B4 | C4 | D4 | E4 | F4 | G4 | H4;
+constexpr Bitboard RANK_5 = A5 | B5 | C5 | D5 | E5 | F5 | G5 | H5;
+constexpr Bitboard RANK_6 = A6 | B6 | C6 | D6 | E6 | F6 | G6 | H6;
+constexpr Bitboard RANK_7 = A7 | B7 | C7 | D7 | E7 | F7 | G7 | H7;
+constexpr Bitboard RANK_8 = A8 | B8 | C8 | D8 | E8 | F8 | G8 | H8;
+
+constexpr Bitboard FILE_A = A1 | A2 | A3 | A4 | A5 | A6 | A7 | A8;
+constexpr Bitboard FILE_B = B1 | B2 | B3 | B4 | B5 | B6 | B7 | B8;
+constexpr Bitboard FILE_C = C1 | C2 | C3 | C4 | C5 | C6 | C7 | C8;
+constexpr Bitboard FILE_D = D1 | D2 | D3 | D4 | D5 | D6 | D7 | D8;
+constexpr Bitboard FILE_E = E1 | E2 | E3 | E4 | E5 | E6 | E7 | E8;
+constexpr Bitboard FILE_F = F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8;
+constexpr Bitboard FILE_G = G1 | G2 | G3 | G4 | G5 | G6 | G7 | G8;
+constexpr Bitboard FILE_H = H1 | H2 | H3 | H4 | H5 | H6 | H7 | H8;
+
+constexpr Bitboard START_RANK_WHITE = RANK_2;
+constexpr Bitboard START_RANK_BLACK = RANK_7;
+constexpr Bitboard PROMOTION_RANKS = RANK_1 | RANK_8;
 
 constexpr std::array<const char* const, 64> SQUARE_LABEL{
     "H1", "G1", "F1", "E1", "D1", "C1", "B1", "A1",  //
--- bitboard/test/squares_unit_tests.cpp
+++ bitboard/test/squares_unit_tests.cpp
@@ -1,8 +1,8 @@
 #include "bitboard/squares.h"
 
-#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
-#include <array>
+#include <vector>
 #include <limits>
 
 namespace Chess
@@ -10,21 +10,42 @@ namespace Chess
 namespace
 {
 
-const std::array<Bitboard, 64> ALL_SQUARES{
-    H1, G1, F1, E1, D1, C1, B1, A1, H2, G2, F2, E2, D2, C2, B2, A2, H3, G3, F3, E3, D3, C3,
-    B3, A3, H4, G4, F4, E4, D4, C4, B4, A4, H5, G5, F5, E5, D5, C5, B5, A5, H6, G6, F6, E6,
-    D6, C6, B6, A6, H7, G7, F7, E7, D7, C7, B7, A7, H8, G8, F8, E8, D8, C8, B8, A8,
+struct SquareTestParameter
+{
+    std::vector<Bitboard> elements;
+    int expected_size;
 };
 
-TEST(SquaresTest, GivenXorOverAllSquaers_ExpectUnderlyingTypeIsMax)
+class SquareTestFixture : public ::testing::TestWithParam<SquareTestParameter>
 {
-    Bitboard xor_over_all_squares = 0;
-    for (const Bitboard square : ALL_SQUARES)
+};
+
+TEST_P(SquareTestFixture, GivenXorOverAllSquaers_ExpectUnderlyingTypeIsMax)
+{
+    EXPECT_THAT(GetParam().elements.size(), GetParam().expected_size);
+
+    Bitboard xor_over_all_elements = 0;
+    Bitboard last_iteration = 0;
+    for (const Bitboard element : GetParam().elements)
     {
-        xor_over_all_squares ^= square;
+        xor_over_all_elements ^= element;
+        EXPECT_FALSE(last_iteration & ~xor_over_all_elements);  // Not bit was unset in this iteration
+        last_iteration = xor_over_all_elements;
     }
-    EXPECT_EQ(xor_over_all_squares, std::numeric_limits<Bitboard>::max());
+    EXPECT_EQ(xor_over_all_elements, std::numeric_limits<Bitboard>::max());
 }
+
+const SquareTestParameter ALL_SQUARES{
+    {H1, G1, F1, E1, D1, C1, B1, A1, H2, G2, F2, E2, D2, C2, B2, A2, H3, G3, F3, E3, D3, C3,
+     B3, A3, H4, G4, F4, E4, D4, C4, B4, A4, H5, G5, F5, E5, D5, C5, B5, A5, H6, G6, F6, E6,
+     D6, C6, B6, A6, H7, G7, F7, E7, D7, C7, B7, A7, H8, G8, F8, E8, D8, C8, B8, A8},
+    64};
+const SquareTestParameter ALL_RANKS{{RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8}, 8};
+const SquareTestParameter ALL_FILES{{FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H}, 8};
+
+INSTANTIATE_TEST_SUITE_P(AllSquares, SquareTestFixture, ::testing::ValuesIn({ALL_SQUARES}));
+INSTANTIATE_TEST_SUITE_P(AllRanks, SquareTestFixture, ::testing::ValuesIn({ALL_RANKS}));
+INSTANTIATE_TEST_SUITE_P(AllFiles, SquareTestFixture, ::testing::ValuesIn({ALL_FILES}));
 
 }  // namespace
 }  // namespace Chess
