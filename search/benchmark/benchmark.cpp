fb09e8068219d2847cfaa2b93e8e24d9412031e9
Sebastian Carter <Sebastian.Carter@web.de>
2020-12-13 13:12:10 +0100

    Simplify benchmark and increase depth
    -----------------------------------------------------------------------
    Benchmark                             Time             CPU   Iterations
    -----------------------------------------------------------------------
    FullSearch/repeats:10_mean         3580 ms         3576 ms           10
    FullSearch/repeats:10_median       3557 ms         3554 ms           10
    FullSearch/repeats:10_stddev       80.0 ms         79.5 ms           10

--- search/benchmark/benchmark.cpp
+++ search/benchmark/benchmark.cpp
@@ -6,35 +6,34 @@
 
 #include <benchmark/benchmark.h>
 
-const std::array<const char* const, 5> fens{"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
-                                            "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1",
-                                            "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
-                                            "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
-                                            "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10"};
+const char* const starting_position_fen = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
+const char* const middle_game_fen = "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10";
+const char* const end_game_fen = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1";
 
 long long Chess::Statistic::number_of_evaluations = 0;
 
 static void FullSearch(benchmark::State& state)
 {
-    Chess::Position position = Chess::PositionFromFen(fens.at(state.range(0)));
     Chess::MoveList move_list{};
+
+    Chess::Position starting_position = Chess::PositionFromFen(starting_position_fen);
+    Chess::Position middle_game = Chess::PositionFromFen(starting_position_fen);
+    Chess::Position end_game = Chess::PositionFromFen(starting_position_fen);
 
     for (auto _ : state)
     {
         Chess::minimax<Chess::SearchAllBranchesWithoutPruning,
                        Chess::GenerateAllPseudoLegalMoves,
-                       Chess::EvaluteToZero>(3, position, move_list.begin());
+                       Chess::EvaluteToZero>(4, starting_position, move_list.begin());
+        Chess::minimax<Chess::SearchAllBranchesWithoutPruning,
+                       Chess::GenerateAllPseudoLegalMoves,
+                       Chess::EvaluteToZero>(4, middle_game, move_list.begin());
+        Chess::minimax<Chess::SearchAllBranchesWithoutPruning,
+                       Chess::GenerateAllPseudoLegalMoves,
+                       Chess::EvaluteToZero>(4, end_game, move_list.begin());
     }
 }
 
-BENCHMARK(FullSearch)
-    ->Arg(0)
-    ->Arg(1)
-    ->Arg(2)
-    ->Arg(3)
-    ->Arg(4)
-    ->Unit(benchmark::kMillisecond)
-    ->ReportAggregatesOnly()
-    ->Repetitions(5);
+BENCHMARK(FullSearch)->Unit(benchmark::kMillisecond)->ReportAggregatesOnly()->Repetitions(10);
 
 BENCHMARK_MAIN();
