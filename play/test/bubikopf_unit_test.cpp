8442d5e338b03ffa207d07894fbca5037013f2e8
Sebastian Carter <Sebastian.Carter@web.de>
2021-01-03 13:25:57 +0100

    Fix tests after moving catch/try

--- play/test/bubikopf_unit_test.cpp
+++ play/test/bubikopf_unit_test.cpp
@@ -19,15 +19,15 @@ class BubikopfTestFixture : public testing::Test
 
 TEST_F(BubikopfTestFixture, GivenGuiMoveListToShort_ExpectThrowsRuntimeError)
 {
-    engine_api.UpdateBoard({"e2e4"});                                                 // Play one move
-    EXPECT_DEATH(engine_api.UpdateBoard({}), "Move list from gui is behind engine");  // To few moves provided
+    engine_api.UpdateBoard({"e2e4"});  // Play one move
+    EXPECT_THROW(engine_api.UpdateBoard({}),
+                 std::runtime_error);  // To few moves provided
 }
 
 TEST_F(BubikopfTestFixture, GivenGuiMoveNotPlayableFromCurrentPosition_ExpectThrowsRuntimeError)
 {
     const auto impossible_move = "a1h7";  // Is equivalent to legal move that can't be played from current position
-    EXPECT_DEATH(engine_api.UpdateBoard({impossible_move}),
-                 "Move played by gui not possible from internal representation");
+    EXPECT_THROW(engine_api.UpdateBoard({impossible_move}), std::runtime_error);
 }
 
 TEST_F(BubikopfTestFixture, DISABLED_GivenSelfPlay_ExpectGameFinishes)
