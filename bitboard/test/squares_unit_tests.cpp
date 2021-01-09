dcde92a32db89b5fbfd2593b7b96066ab6c75021
Sebastian Carter <Sebastian.Carter@web.de>
2021-01-09 18:39:30 +0100

    Fix formatting of constexpr arrays

--- bitboard/test/squares_unit_tests.cpp
+++ bitboard/test/squares_unit_tests.cpp
@@ -35,17 +35,17 @@ TEST_P(SquareTestFixture, GivenXorOverAllSquaers_ExpectUnderlyingTypeIsMax)
     EXPECT_EQ(xor_over_all_elements, std::numeric_limits<Bitboard>::max());
 }
 
-const SquareTestParameter ALL_SQUARES{
+const SquareTestParameter kAllSquaresSample{
     {H1, G1, F1, E1, D1, C1, B1, A1, H2, G2, F2, E2, D2, C2, B2, A2, H3, G3, F3, E3, D3, C3,
      B3, A3, H4, G4, F4, E4, D4, C4, B4, A4, H5, G5, F5, E5, D5, C5, B5, A5, H6, G6, F6, E6,
      D6, C6, B6, A6, H7, G7, F7, E7, D7, C7, B7, A7, H8, G8, F8, E8, D8, C8, B8, A8},
     64};
-const SquareTestParameter ALL_RANKS{{kRank1, kRank2, kRank3, kRank4, kRank5, kRank6, kRank7, kRank8}, 8};
-const SquareTestParameter ALL_FILES{{kFileA, kFileB, kFileC, kFileD, kFileE, kFileF, kFileG, kFileH}, 8};
+const SquareTestParameter kAllRanks{{kRank1, kRank2, kRank3, kRank4, kRank5, kRank6, kRank7, kRank8}, 8};
+const SquareTestParameter kAllFiles{{kFileA, kFileB, kFileC, kFileD, kFileE, kFileF, kFileG, kFileH}, 8};
 
-INSTANTIATE_TEST_SUITE_P(AllSquares, SquareTestFixture, testing::Values(ALL_SQUARES));
-INSTANTIATE_TEST_SUITE_P(AllRanks, SquareTestFixture, testing::Values(ALL_RANKS));
-INSTANTIATE_TEST_SUITE_P(AllFiles, SquareTestFixture, testing::Values(ALL_FILES));
+INSTANTIATE_TEST_SUITE_P(AllSquares, SquareTestFixture, testing::Values(kAllSquaresSample));
+INSTANTIATE_TEST_SUITE_P(AllRanks, SquareTestFixture, testing::Values(kAllRanks));
+INSTANTIATE_TEST_SUITE_P(AllFiles, SquareTestFixture, testing::Values(kAllFiles));
 
 }  // namespace
 }  // namespace Chess
