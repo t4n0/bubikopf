ca4221ff31e0edc7f1ba95c1399c5b1bdb464364
Sebastian Carter <Sebastian.Carter@web.de>
2021-01-10 15:10:45 +0100

    Use LoopOverAllSquares for calculating knight attacks

--- bitboard/lookup_table/knight.h
+++ bitboard/lookup_table/knight.h
@@ -51,49 +51,28 @@ constexpr std::array<Bitboard, 8> kLegalLandingAreasWithoutWrapping{~(kRank1 | k
 /// @brief Creates a bitboard from source_bit, shifts it in direction and NULLs
 /// the board if "wrap around" occurs.
 /// @pre Assumes that only a single bit is set
-template <int source_bit, std::size_t direction>
+template <Bitboard source, std::size_t direction>
 struct KnightJump
 {
     constexpr static Bitboard value =
         std::get<direction>(kLegalLandingAreasWithoutWrapping) &
-        (std::get<direction>(kJumpBits) > 0
-             ? ToBitboard<source_bit>::value << std::get<direction>(kJumpBits)
-             : ToBitboard<source_bit>::value >> Absolute<std::get<direction>(kJumpBits)>::value);
+        (std::get<direction>(kJumpBits) > 0 ? source << std::get<direction>(kJumpBits)
+                                            : source >> Absolute<std::get<direction>(kJumpBits)>::value);
 };
 
-template <int source_bit, std::size_t direction = 0>
+template <Bitboard source, std::size_t direction = 0>
 struct KnightJumps
 {
-    constexpr static Bitboard value =
-        KnightJump<source_bit, direction>::value | KnightJumps<source_bit, direction + 1>::value;
+    constexpr static Bitboard value = KnightJump<source, direction>::value | KnightJumps<source, direction + 1>::value;
 };
 
-template <int source_bit>
-struct KnightJumps<source_bit, 8>
+template <Bitboard source>
+struct KnightJumps<source, 7>
 {
-    constexpr static Bitboard value = 0;
+    constexpr static Bitboard value = KnightJump<source, 7>::value;
 };
 
-/// @brief When accessing this lookup table with the source bit as index (e.g. 0
-/// is H1 , 7 is A1, etc.) a bitboard is returned with all possible landing
-/// squares for a knight jump
-constexpr std::array<Bitboard, 64> kKnightJumps{
-    KnightJumps<0>::value,  KnightJumps<1>::value,  KnightJumps<2>::value,  KnightJumps<3>::value,
-    KnightJumps<4>::value,  KnightJumps<5>::value,  KnightJumps<6>::value,  KnightJumps<7>::value,
-    KnightJumps<8>::value,  KnightJumps<9>::value,  KnightJumps<10>::value, KnightJumps<11>::value,
-    KnightJumps<12>::value, KnightJumps<13>::value, KnightJumps<14>::value, KnightJumps<15>::value,
-    KnightJumps<16>::value, KnightJumps<17>::value, KnightJumps<18>::value, KnightJumps<19>::value,
-    KnightJumps<20>::value, KnightJumps<21>::value, KnightJumps<22>::value, KnightJumps<23>::value,
-    KnightJumps<24>::value, KnightJumps<25>::value, KnightJumps<26>::value, KnightJumps<27>::value,
-    KnightJumps<28>::value, KnightJumps<29>::value, KnightJumps<30>::value, KnightJumps<31>::value,
-    KnightJumps<32>::value, KnightJumps<33>::value, KnightJumps<34>::value, KnightJumps<35>::value,
-    KnightJumps<36>::value, KnightJumps<37>::value, KnightJumps<38>::value, KnightJumps<39>::value,
-    KnightJumps<40>::value, KnightJumps<41>::value, KnightJumps<42>::value, KnightJumps<43>::value,
-    KnightJumps<44>::value, KnightJumps<45>::value, KnightJumps<46>::value, KnightJumps<47>::value,
-    KnightJumps<48>::value, KnightJumps<49>::value, KnightJumps<50>::value, KnightJumps<51>::value,
-    KnightJumps<52>::value, KnightJumps<53>::value, KnightJumps<54>::value, KnightJumps<55>::value,
-    KnightJumps<56>::value, KnightJumps<57>::value, KnightJumps<58>::value, KnightJumps<59>::value,
-    KnightJumps<60>::value, KnightJumps<61>::value, KnightJumps<62>::value, KnightJumps<63>::value};
+constexpr auto kKnightJumps = LoopOverAllSquares<KnightJumps>();
 
 }  // namespace Chess
 
--- bitboard/lookup_table/utilities.h
+++ bitboard/lookup_table/utilities.h
@@ -15,13 +15,6 @@ struct Absolute
     constexpr static int value = positive_or_negative < 0 ? -positive_or_negative : positive_or_negative;
 };
 
-/// @brief Converts input source_bit to a bitboard
-template <int source_bit>
-struct ToBitboard
-{
-    constexpr static Bitboard value = Bitboard{1} << source_bit;
-};
-
 /// @brief A function mimicking a for loop over all squares at compile time.
 ///
 /// It calls itself recursively. The final call fills value at index 63. The second to last index 62, etc.
