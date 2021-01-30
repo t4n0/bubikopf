560dfcc72fe649bd2f952e1fc35615b771e2460f
Sebastian Carter <Sebastian.Carter@web.de>
2021-01-30 17:07:29 +0100

    Fix bug in benchmark setup
    ------------------------------------------------------------------------------
    Benchmark                                    Time             CPU   Iterations
    ------------------------------------------------------------------------------
    TraverseAllLeaves/repeats:10_mean          341 ms          341 ms           10
    TraverseAllLeaves/repeats:10_median        341 ms          341 ms           10
    TraverseAllLeaves/repeats:10_stddev       2.73 ms         2.73 ms           10
    FindBestMove/repeats:10_mean               235 ms          235 ms           10
    FindBestMove/repeats:10_median             232 ms          232 ms           10
    FindBestMove/repeats:10_stddev            7.16 ms         7.16 ms           10

--- search/benchmark/benchmark.cpp
+++ search/benchmark/benchmark.cpp
@@ -6,52 +6,54 @@
 
 #include <benchmark/benchmark.h>
 
-const char* const starting_position_fen = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
-const char* const middle_game_fen = "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10";
-const char* const end_game_fen = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1";
+namespace
+{
 
-static void TraverseAllLeavesDepth4(benchmark::State& state)
+const char* const kStartPositionFen = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
+const char* const kMiddleGameFen = "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10";
+const char* const kEndGameFen = "8/2p5/1P1p4/2P3rk/KR3p2/4P1p1/5P2/8 w - - 0 1";
+constexpr int kDepth4 = 4;
+constexpr int kDepth5 = 5;
+constexpr int kDepth6 = 6;
+constexpr Chess::Evaluation kNegamaxEvaluationSignWhite{1};
+
+}  // namespace
+
+static void TraverseAllLeaves(benchmark::State& state)
 {
     Chess::MoveStack move_stack{};
     Chess::Statistic stats{};
-    constexpr int depth{4};
-
-    Chess::Position starting_position = Chess::PositionFromFen(starting_position_fen);
-    Chess::Position middle_game = Chess::PositionFromFen(starting_position_fen);
-    Chess::Position end_game = Chess::PositionFromFen(starting_position_fen);
+    Chess::Position start_position = Chess::PositionFromFen(kStartPositionFen);
+    Chess::Position middle_game = Chess::PositionFromFen(kMiddleGameFen);
+    Chess::Position end_game = Chess::PositionFromFen(kEndGameFen);
 
     for (auto _ : state)
     {
         Chess::TraverseAllLeaves<Chess::GenerateAllPseudoLegalMoves>(
-            starting_position, depth, move_stack.begin(), stats);
-        Chess::TraverseAllLeaves<Chess::GenerateAllPseudoLegalMoves>(middle_game, depth, move_stack.begin(), stats);
-        Chess::TraverseAllLeaves<Chess::GenerateAllPseudoLegalMoves>(end_game, depth, move_stack.begin(), stats);
+            start_position, kDepth4, move_stack.begin(), stats);
+        Chess::TraverseAllLeaves<Chess::GenerateAllPseudoLegalMoves>(middle_game, kDepth4, move_stack.begin(), stats);
+        Chess::TraverseAllLeaves<Chess::GenerateAllPseudoLegalMoves>(end_game, kDepth5, move_stack.begin(), stats);
     }
 }
+BENCHMARK(TraverseAllLeaves)->Unit(benchmark::kMillisecond)->ReportAggregatesOnly()->Repetitions(10);
 
-BENCHMARK(TraverseAllLeavesDepth4)->Unit(benchmark::kMillisecond)->ReportAggregatesOnly()->Repetitions(10);
-
-static void FindBestMoveDepth6(benchmark::State& state)
+static void FindBestMove(benchmark::State& state)
 {
     Chess::MoveStack move_stack{};
-    constexpr int depth{6};
-    constexpr Chess::Evaluation negamax_sign_white{1};
-
-    Chess::Position starting_position = Chess::PositionFromFen(starting_position_fen);
-    Chess::Position middle_game = Chess::PositionFromFen(starting_position_fen);
-    Chess::Position end_game = Chess::PositionFromFen(starting_position_fen);
+    Chess::Position start_position = Chess::PositionFromFen(kStartPositionFen);
+    Chess::Position middle_game = Chess::PositionFromFen(kMiddleGameFen);
+    Chess::Position end_game = Chess::PositionFromFen(kEndGameFen);
 
     for (auto _ : state)
     {
         Chess::FindBestMove<Chess::GenerateAllPseudoLegalMoves, Chess::EvaluateMaterial>(
-            starting_position, depth, move_stack.begin(), negamax_sign_white);
+            start_position, kDepth6, move_stack.begin(), kNegamaxEvaluationSignWhite);
         Chess::FindBestMove<Chess::GenerateAllPseudoLegalMoves, Chess::EvaluateMaterial>(
-            middle_game, depth, move_stack.begin(), negamax_sign_white);
+            middle_game, kDepth6, move_stack.begin(), kNegamaxEvaluationSignWhite);
         Chess::FindBestMove<Chess::GenerateAllPseudoLegalMoves, Chess::EvaluateMaterial>(
-            end_game, depth, move_stack.begin(), negamax_sign_white);
+            end_game, kDepth6, move_stack.begin(), kNegamaxEvaluationSignWhite);
     }
 }
-
-BENCHMARK(FindBestMoveDepth6)->Unit(benchmark::kMillisecond)->ReportAggregatesOnly()->Repetitions(10);
+BENCHMARK(FindBestMove)->Unit(benchmark::kMillisecond)->ReportAggregatesOnly()->Repetitions(10);
 
 BENCHMARK_MAIN();
